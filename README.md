99bb5d53774d1c24b438282e116844f5b28d409d
Rose Liu <rose@byanyothername.me>
2026-08-08 02:32:04 -0400

    png to bmp coversion script

--- README.md
+++ README.md
@@ -3,12 +3,7 @@ A system of dashboards that I'm probably gonna overthink and overengineer
 
 ## Requirements
 
-- For generating the dashboards
-    - npm
-
-- (Future) For pre-rendering to display on basic devices:
-    - Headless chrome
-    - Image Magick
+Through NPM, all things are possible
 
 ## TODO
 
---
+++ scripts/converttobmp.js
@@ -0,0 +1,20 @@
+import path from 'node:path';
+import { Jimp } from "jimp";
+
+const args = process.argv.slice(2);
+
+if (args.length < 1) {
+    console.error("Expected argument for screenshot to convert");
+    process.exit(1);
+}
+
+const dashboard = args[0];
+
+const pngPath = path.resolve(import.meta.dirname, `../renders/${dashboard}.png`);
+const bmpPath = path.resolve(import.meta.dirname, `../renders/${dashboard}.bmp`);
+
+const image = await Jimp.read(pngPath);
+
+// todo: grayscale and dither?
+
+await image.write(bmpPath);
